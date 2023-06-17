7b81934067c8ff42199965fb0b0550145c2e784b
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2023-06-17 09:07:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function initializeGame() {
         initiateLevel = randomNumber();
         setTimeout(function () { flashButton(initiateLevel) }, 200);
         computerResponse.push(initiateLevel);
-        // console.log("Computer response in initialize game is: " + computerResponse);
+        ////console.log("Computer response in initialize game is: " + computerResponse);
         clickDetect();
     }, { once: true })
 }
@@ -75,100 +75,111 @@ function initializeGame() {
 function clickDetect() {
     var arrayOfAllButtons = document.querySelectorAll(".btn");
     for (let j = 0; j < 4; j++) {
-        arrayOfAllButtons[j].addEventListener("click", function () {
-            var arrayOfButtonClass = Array.from(this.classList);
-
-            //Play sound when buttons are clicked
-            if (arrayOfButtonClass.includes("green")) {
-                // console.log("Entered click detect 1");
-                userResponse.push(1);
-                flashButton(1);
-                if (userResponse.length < computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
-                    for (let s = 0; s < userResponse.length; s++) {
-                        if (userResponse[s] !== computerResponse[s]) {
-                            // console.log("Entered the if in clickDetect");
-                            continueGame(userResponse, computerResponse);
-                            // console.log("Exited the if in clickDetect");
-                        }
-                    }
-                }
-
-                else if (userResponse.length === computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
-                    // console.log("ENTERED EQUAL LENGTH IF");
-                    continueGame(userResponse, computerResponse);
-                    // console.log("EXITED EQUAL LENGTH IF");
-                }
-                // console.log("Exited click detect 1");
-            }
-
-            if (arrayOfButtonClass.includes("red")) {
-                // console.log("Entered click detect 2");
-                userResponse.push(2);
-                flashButton(2);
-                if (userResponse.length < computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
-                    for (let s = 0; s < userResponse.length; s++) {
-                        if (userResponse[s] !== computerResponse[s]) {
-                            // console.log("Entered the if in clickDetect");
-                            continueGame(userResponse, computerResponse);
-                            // console.log("Exited the if in clickDetect");
-                        }
-                    }
-                }
-
-                else if (userResponse.length === computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
-                    // console.log("ENTERED EQUAL LENGTH IF");
-                    continueGame(userResponse, computerResponse);
-                    // console.log("EXITED EQUAL LENGTH IF");
-                }
-                // console.log("Exited click detect 2");
-            }
-
-            if (arrayOfButtonClass.includes("yellow")) {
-                // console.log("Entered click detect 3");
-                userResponse.push(3);
-                flashButton(3);
-                if (userResponse.length < computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
-                    for (let s = 0; s < userResponse.length; s++) {
-                        if (userResponse[s] !== computerResponse[s]) {
-                            // console.log("Entered the if in clickDetect");
-                            continueGame(userResponse, computerResponse);
-                            // console.log("Exited the if in clickDetect");
-                        }
-                    }
-                }
-
-                else if (userResponse.length === computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
-                    // console.log("ENTERED EQUAL LENGTH IF");
-                    continueGame(userResponse, computerResponse);
-                    // console.log("EXITED EQUAL LENGTH IF");
-                }
-                // console.log("Exited click detect 3");
-            }
-
-            if (arrayOfButtonClass.includes("blue")) {
-                // console.log("Entered click detect 4");
-                userResponse.push(4);
-                flashButton(4);
-                if (userResponse.length < computerResponse.length && userResponse.length !== 0) {
-                    for (let s = 0; s < userResponse.length; s++) {
-                        if (userResponse[s] !== computerResponse[s]) {
-                            // console.log("Entered the if in clickDetect");
-                            continueGame(userResponse, computerResponse);
-                            // console.log("Exited the if in clickDetect");
-                        }
-                    }
-                }
-
-                else if (userResponse.length === computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
-                    // console.log("ENTERED EQUAL LENGTH IF");
-                    continueGame(userResponse, computerResponse);
-                    // console.log("EXITED EQUAL LENGTH IF");
-                }
-                // console.log("Exited click detect 4");
-            }
-
-        })
+        arrayOfAllButtons[j].addEventListener("click", whatHappensWhenYouClick)
     }
+}
+
+//Callback function for adding and removing event listener
+function whatHappensWhenYouClick() {
+    var arrayOfButtonClass = Array.from(this.classList);
+
+    //Play sound when buttons are clicked
+    if (arrayOfButtonClass.includes("green")) {
+        ////console.log("Entered click detect 1");
+        userResponse.push(1);
+        flashButton(1);
+        if (userResponse.length < computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
+            for (let s = 0; s < userResponse.length; s++) {
+                if (userResponse[s] !== computerResponse[s]) {
+                    //console.log("Entered the if in clickDetect");
+                    continueGame(userResponse, computerResponse);
+                    //console.log("Exited the if in clickDetect");
+                }
+            }
+        }
+
+        else if (userResponse.length === computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
+            //console.log("ENTERED EQUAL LENGTH IF");
+            continueGame(userResponse, computerResponse);
+            //console.log("EXITED EQUAL LENGTH IF");
+        }
+        //console.log("Exited click detect 1");
+    }
+
+    if (arrayOfButtonClass.includes("red")) {
+        //console.log("Entered click detect 2");
+        userResponse.push(2);
+        flashButton(2);
+        if (userResponse.length < computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
+            for (let s = 0; s < userResponse.length; s++) {
+                if (userResponse[s] !== computerResponse[s]) {
+                    //console.log("Entered the if in clickDetect");
+                    continueGame(userResponse, computerResponse);
+                    //console.log("Exited the if in clickDetect");
+                }
+            }
+        }
+
+        else if (userResponse.length === computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
+            //console.log("ENTERED EQUAL LENGTH IF");
+            continueGame(userResponse, computerResponse);
+            //console.log("EXITED EQUAL LENGTH IF");
+        }
+        //console.log("Exited click detect 2");
+    }
+
+    if (arrayOfButtonClass.includes("yellow")) {
+        //console.log("Entered click detect 3");
+        userResponse.push(3);
+        flashButton(3);
+        if (userResponse.length < computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
+            for (let s = 0; s < userResponse.length; s++) {
+                if (userResponse[s] !== computerResponse[s]) {
+                    //console.log("Entered the if in clickDetect");
+                    continueGame(userResponse, computerResponse);
+                    //console.log("Exited the if in clickDetect");
+                }
+            }
+        }
+
+        else if (userResponse.length === computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
+            //console.log("ENTERED EQUAL LENGTH IF");
+            continueGame(userResponse, computerResponse);
+            //console.log("EXITED EQUAL LENGTH IF");
+        }
+        //console.log("Exited click detect 3");
+    }
+
+    if (arrayOfButtonClass.includes("blue")) {
+        //console.log("Entered click detect 4");
+        userResponse.push(4);
+        flashButton(4);
+        if (userResponse.length < computerResponse.length && userResponse.length !== 0) {
+            for (let s = 0; s < userResponse.length; s++) {
+                if (userResponse[s] !== computerResponse[s]) {
+                    //console.log("Entered the if in clickDetect");
+                    continueGame(userResponse, computerResponse);
+                    //console.log("Exited the if in clickDetect");
+                }
+            }
+        }
+
+        else if (userResponse.length === computerResponse.length && userResponse.length !== 0 && computerResponse.length !== 0) {
+            //console.log("ENTERED EQUAL LENGTH IF");
+            continueGame(userResponse, computerResponse);
+            //console.log("EXITED EQUAL LENGTH IF");
+        }
+        //console.log("Exited click detect 4");
+    }
+
+}
+
+//Remove Click Detect
+function removeClickDetect(){
+var arrayOfAllButtons = document.querySelectorAll(".btn");
+for (let j = 0; j < 4; j++) {
+    arrayOfAllButtons[j].removeEventListener("click", whatHappensWhenYouClick)
+}
 }
 
 //Array Equality Checker
@@ -196,28 +207,28 @@ function arrayEqualityCheck(array1, array2) {
 function continueGame(user$Response, computer$Response) {
 
     if (arrayEqualityCheck(user$Response, computer$Response)) {
-        // console.log("Entered 1st if");
+        //console.log("Entered 1st if");
         var storeNumber = randomNumber();
-        // console.log("Computer response before push " + computer$Response);
-        // console.log("User response before clearing " + user$Response);
+        //console.log("Computer response before push " + computer$Response);
+        //console.log("User response before clearing " + user$Response);
         document.querySelector("h1").textContent = "Level " + [i];
-        // console.log("i is: " + i);
+        //console.log("i is: " + i);
         i++;
-        // console.log("i++ is: " + i);
+        // //console.log("i++ is: " + i);
         setTimeout(function () { flashButton(storeNumber) }, 700);
         user$Response.length = 0;
-        // console.log("User response after clearing " + user$Response);
+        //console.log("User response after clearing " + user$Response);
         computer$Response.push(storeNumber);
-        // console.log("Computer response after push " + computer$Response);
-        // console.log("Exiting 1st if");
+        //console.log("Computer response after push " + computer$Response);
+        //console.log("Exiting 1st if");
     }
 
     if (!(arrayEqualityCheck(user$Response, computer$Response)) && user$Response.length !== 0) {
-        // console.log("Entered other if");
-        // console.log("Other if is running :- User response: " + user$Response + " Computer Response: " + computer$Response)
+        //console.log("Entered other if");
+        //console.log("Other if is running :- User response: " + user$Response + " Computer Response: " + computer$Response)
         computerResponse.length = 0;
         userResponse.length = 0;
-        // console.log("Other if is running after clearing :- User response: " + user$Response + " Computer Response: " + computer$Response)
+        //console.log("Other if is running after clearing :- User response: " + user$Response + " Computer Response: " + computer$Response)
         document.querySelector("h1").textContent = "Game Over, Press Any Key To Restart";
 
         document.querySelector("body").classList.add("game-over");
@@ -226,11 +237,12 @@ function continueGame(user$Response, computer$Response) {
             audio.play();
             document.querySelector("body").classList.remove("game-over");
         }, 100)
-        // console.log("User response in other if at last is: " + user$Response);
-        // console.log("Computer response in other if at last is: " + computer$Response);
-        // console.log("Exiting other if");
+        //console.log("User response in other if at last is: " + user$Response);
+        //console.log("Computer response in other if at last is: " + computer$Response);
+        //console.log("Exiting other if");
         i = 2;
-        // console.log("i is reset and should be 2, is it? " + i)
+        //console.log("i is reset and should be 2, is it? " + i);
+        removeClickDetect();
         initializeGame();
     }
 }
